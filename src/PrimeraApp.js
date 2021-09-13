34de510fd6122ddddc72a7b9f81e8fdd6eaaa1a1
Kneyder <Kevinneyderbutronm@gmail.com>
2021-09-12 22:01:38 -0400

    commit

---
+++ src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PrimeraApp = ( {saludo} ) => {
+
+    // console.log(saludo);
+    return (
+        <>
+            <h1>Helloo, {saludo}</h1>
+            <p>Primer parrafo</p>
+        </>
+    );
+};
+
+export default PrimeraApp;
